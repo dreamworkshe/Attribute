c48199fd3b4c4e50ce9a22427365915071c30e13
Yao-Cheng Li <dreamworkshe@gmail.com>
2013-02-18 15:56:17 -0800

    Lecture finish version

--- Attribute/AttributeViewController.m
+++ Attribute/AttributeViewController.m
@@ -16,11 +16,42 @@
 
 @implementation AttributeViewController
 
-- (IBAction)underline {
-    NSRange range = [[self.label.attributedText string] rangeOfString:[self selectedWord]];
+- (void)addLabelAttributes:(NSDictionary *)attributes range:(NSRange)range {
     if (range.location != NSNotFound) {
-        
+        NSMutableAttributedString *mat = [self.label.attributedText mutableCopy];
+        [mat addAttributes:attributes
+                     range:range];
+        self.label.attributedText = mat;
     }
+}
+
+- (void)addSelectedWordAttributes:(NSDictionary *)attributes {
+    NSRange range = [[self.label.attributedText string] rangeOfString:[self selectedWord]];
+    [self addLabelAttributes:attributes range:range];
+}
+
+- (IBAction)underline {
+    [self addSelectedWordAttributes:@{ NSUnderlineStyleAttributeName: @(NSUnderlineStyleSingle) }];
+}
+
+- (IBAction)ununderline {
+    [self addSelectedWordAttributes:@{ NSUnderlineStyleAttributeName: @(NSUnderlineStyleNone) }];
+}
+
+- (IBAction)changeColor:(UIButton *)sender {
+    [self addSelectedWordAttributes:@{ NSForegroundColorAttributeName: sender.backgroundColor }];
+}
+
+- (IBAction)changeFont:(UIButton *)sender {
+    
+    CGFloat fontSize = [UIFont systemFontSize];
+    
+    NSDictionary *attributes = [self.label.attributedText attributesAtIndex:0 effectiveRange:NULL];
+    UIFont *existingFont = attributes[NSFontAttributeName];
+    if (existingFont) fontSize = existingFont.pointSize;
+    
+    UIFont *font = [sender.titleLabel.font fontWithSize:fontSize];
+    [self addSelectedWordAttributes:@{ NSFontAttributeName: font }];
 }
 
 
